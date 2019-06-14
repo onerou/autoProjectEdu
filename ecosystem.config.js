df184448eedc16e615f10891cd7a3144d8e8c820
onerou <1057674027@qq.com>
2019-06-14 15:37:13 +0800

    新增pm2 托管配置，新增检查是否阅读功能

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps : [{
+    name: 'autoProjectEdu',
+    script: 'app.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
--- puppeteerDo.js
+++ puppeteerDo.js
@@ -1,87 +1,86 @@
 const puppeteer = require('puppeteer');
-var urlList = [];
+var urlList = [],
+	t;
 const goFn = async () => {
-    global.browser = await puppeteer.launch({
-        defaultViewport: {
-            width: 1400,
-            height: 930
-        },
-        // devtools: true,
-        // headless: false,
-        headless: true
-    });
-    global.page = 0;
-    const login = await browser.newPage();
-    await login.goto('http://edu.piesat.cn/login.htm');
-    await doLogin(login);
+	global.browser = await puppeteer.launch({
+		defaultViewport: {
+			width: 1400,
+			height: 930
+		},
+		// devtools: true,
+		// headless: false
+		headless: true
+	});
+	global.page = 0;
+	const login = await browser.newPage();
+	await login.goto('http://edu.piesat.cn/login.htm');
+	await doLogin(login);
 
-    // })
+	// })
 };
 const goList = async () => {
-    global.page++;
-    await global.list.goto(
-        'http://edu.piesat.cn/kng/knowledgecatalogsearch.htm?t=DocumentKnowledge&sf=RecommendLevel&s=dc&ps=50&pi=' +
-        global.page
-    );
-    await doList(list);
+	global.page++;
+	await global.list.goto(
+		'http://edu.piesat.cn/kng/knowledgecatalogsearch.htm?t=DocumentKnowledge&sf=RecommendLevel&s=dc&ps=50&pi=' +
+			global.page
+	);
+	await doList(list);
 };
 const doLogin = async (login) => {
-    const loginForm = await login.$('#dvUserNameLoginPanel');
-    await loginForm.$eval('#txtUserName2', (userInput) => (userInput.value = 'hecheng'));
-    await loginForm.$eval('#txtPassword2', (passInput) => (passInput.value = 'Hc199406170037'));
-    await loginForm.$eval('#btnLogin2', (loginBtn) => loginBtn.click());
-    setTimeout(async () => {
-        global.list = await global.browser.newPage();
-        await goList();
-    }, 2000);
+	const loginForm = await login.$('#dvUserNameLoginPanel');
+	await loginForm.$eval('#txtUserName2', (userInput) => (userInput.value = 'zhoubin'));
+	await loginForm.$eval('#txtPassword2', (passInput) => (passInput.value = 'bin100411'));
+	await loginForm.$eval('#btnLogin2', (loginBtn) => loginBtn.click());
+	setTimeout(async () => {
+		global.list = await global.browser.newPage();
+		await goList();
+	}, 2000);
 };
 const doList = async (list) => {
-    const listDiv = await list.$('.el-kng-img-list');
-    if (!listDiv) return;
-    urlList = await list.evaluate(() => {
-        let list = [...document.querySelectorAll('.el-placehold-body')];
-        return list.map((v) => {
-            return 'http://edu.piesat.cn' + v.getAttribute('onclick').split("'")[1];
-        });
-    });
-    await goDatePage(urlList);
+	const listDiv = await list.$('.el-kng-img-list');
+	if (!listDiv) return;
+	urlList = await list.evaluate(() => {
+		let list = [ ...document.querySelectorAll('.el-placehold-body') ];
+		return list.map((v) => {
+			return 'http://edu.piesat.cn' + v.getAttribute('onclick').split("'")[1];
+		});
+	});
+	await goDatePage(urlList);
 };
 const goDatePage = async (list) => {
-    if (list.length == 0) {
-        goList();
-    }
-    let url = list.shift();
-    if (typeof url != 'string') {
-        console.log("TCL: goDatePage -> url", url)
-        datePage.close();
-        await goDatePage(list);
-        return
-    }
-    let datePage = await global.browser.newPage();
-    await datePage.goto(url);
-    await datePage.evaluate(() => {
-        // let i = 0;
-        // let promis = [];
-        // while (i < 20) {
-        //     promis.push(
-        //         new Promise((r) => {
-        //             setTimeout(() => {
-        //                 submitStudy();
-        //                 setTimeout(() => {
-        //                     r();
-        //                 }, 1000);
-        //             }, 1000);
-        //         })
-        //     );
-        //     i++;
-        // }
-        setInterval(() => {
-            submitStudy();
-        }, 1000)
-    });
-    await setTimeout(async () => {
-        datePage.close();
-        await goDatePage(list);
-    }, 30000);
+	try {
+		t && clearInterval(t);
+		if (list.length == 0) {
+			goList();
+		}
+		let url = list.shift();
+		if (!url || typeof url != 'string') {
+			console.log('TCL: goDatePage -> url', url);
+			datePage.close();
+			await goDatePage(list);
+			return;
+		}
+		let datePage = await global.browser.newPage();
+		await datePage.goto(url);
+		t = setInterval(async () => {
+			let flag = await datePage.evaluate(() => {
+				return $('#ScheduleText').text() == '100%';
+			});
+			if (flag) {
+				await datePage.close();
+				setTimeout(async () => {
+					clearInterval(t);
+					await goDatePage(list);
+				});
+				return;
+			} else {
+				await datePage.evaluate(() => {
+					submitStudy();
+				});
+			}
+		}, 1000);
+	} catch (err) {
+		console.log(err);
+	}
 };
 module.exports = goFn;
