8be7ee129f3101b0de5dec9ad30e976860c7f77d
oner <1057674027@qq.com>
2019-10-30 09:58:08 +0800

    分离视频播放以及文档刷新

--- CourseKnowledgeDo.js
+++ CourseKnowledgeDo.js
@@ -60,8 +60,8 @@ const goList = async () => {
 }
 const doLogin = async (login, config) => {
 	const loginForm = await login.$('#dvUserNameLoginPanel')
-	await loginForm.$eval('#txtUserName2', (userInput) => (userInput.value = 'hecheng')) // 用户名
-	await loginForm.$eval('#txtPassword2', (passInput) => (passInput.value = 'Hc199406170037')) // 密码
+	await loginForm.$eval('#txtUserName2', (userInput) => (userInput.value = 'lidan')) // 用户名
+	await loginForm.$eval('#txtPassword2', (passInput) => (passInput.value = '123456')) // 密码
 	await loginForm.$eval('#btnLogin2', (loginBtn) => loginBtn.click())
 	setTimeout(async () => {
 		global.list = await global.browser.newPage()
--- puppeteerDo.js
+++ puppeteerDo.js
@@ -7,7 +7,7 @@ const path = require('path')
 let number = 0
 var urlList = [],
 	t,
-	listType = 'Video',
+	listType = 'Document',
 	pagenumber = 11,
 	pageUrl = {
 		Document: 'DocumentKnowledge',
@@ -58,8 +58,8 @@ const goList = async () => {
 }
 const doLogin = async (login, config) => {
 	const loginForm = await login.$('#dvUserNameLoginPanel')
-	await loginForm.$eval('#txtUserName2', (userInput) => (userInput.value = 'lidan')) // 用户名
-	await loginForm.$eval('#txtPassword2', (passInput) => (passInput.value = '123456')) // 密码
+	await loginForm.$eval('#txtUserName2', (userInput) => (userInput.value = '用户名')) // 用户名
+	await loginForm.$eval('#txtPassword2', (passInput) => (passInput.value = '密码')) // 密码
 	await loginForm.$eval('#btnLogin2', (loginBtn) => loginBtn.click())
 	setTimeout(async () => {
 		global.list = await global.browser.newPage()
